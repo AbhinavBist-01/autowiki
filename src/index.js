0b4f524da8350b0bb7a2f18abec4555d15195b60
Abhinav <abhinavbist05@gmail.com>
2026-09-03 20:05:23 +0530

    feat: integrate Inngest for serverless functions
    - Added Inngest as a dependency in package.json.
    - Configured Inngest client in src/inggest/client.js.
    - Created helloWorld function in src/inggest/functions/helloworld.js with a test trigger.
    - Updated server setup in src/index.js to serve Inngest functions.
    - Exported Inngest client and functions from src/inggest/index.js.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import express from "express";
+import "dotenv/config";
+
+import { inngest, functions } from "./inggest/index.js";
+import { serve } from "inngest/express";
 
 const app = express();
-app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
